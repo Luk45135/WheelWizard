bf7a52d06673c781e8b58c8f7f650d0d0e716f2c
LostLukas <lukasdorji@gmail.com>
2025-05-09 21:35:35 +0200

    add: desktop entry

--- default.nix
+++ default.nix
@@ -2,7 +2,8 @@
   lib,
   buildDotnetModule,
   dotnetCorePackages,
-  ...
+  makeDesktopItem,
+  icoutils,
 }:
 buildDotnetModule {
   pname = "WheelWizard";
@@ -19,8 +20,32 @@ buildDotnetModule {
   dotnetBuildFlags = [
     "-p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true -p:EnableCompressionInSingleFile=true"
   ];
+
   executeables = ["WheelWizard"];
   useAppHost = true;
+
+  postInstall =
+    ''
+      icotool --icon -x car-wheel.ico
+      for i in 16 32 48 256; do
+        size=''${i}x''${i}
+        install -Dm444 *_''${size}x32.png $out/share/icons/hicolor/$size/apps/car-wheel.png
+      done
+    '';
+
+  desktopItems = makeDesktopItem {
+    name = "wheelwizard";
+    genericname = "Mario Kart Wii Mod Manager";
+    comment = "Mario Kart Wii Mod Manager & Retro Rewind Auto Updater";
+    desktopName = "WheelWizard";
+    exec = "WheelWizard";
+    icon = "car-wheel";
+    type = "Application";
+    terminal = false;
+    categories = [
+      "Game"
+    ];
+  };
 }
 
 
